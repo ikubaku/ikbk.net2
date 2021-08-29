5154963f16cfa9cd04919125591de9963dca4987
ikubaku <hide4d51@gmail.com>
2021-08-29 18:56:44 +0900

    Add posts generation from Asciidoc

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,29 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'ikbk.net',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Post',
+        path: './posts/*.adoc',
+        asciidoc: {
+
+        },
+      },
+    },
+  ],
+  templates: {
+    Post: '/posts/:slug'
+  },
+  transformers: {
+    asciidoc: {
+      safe: 'safe',
+      icons: 'font',
+      attributes: {
+        imagesdir: '/assets/img',
+      },
+    },
+  },
 }
---
+++ src/templates/Post.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 v-html="$page.post.title" />
+    <div v-html="$page.post.content" />
+  </div>
+</template>
+
+<page-query>
+query ($id: ID!) {
+  post(id: $id) {
+    title
+    content
+  }
+}
+</page-query>
+
+<script>
+export default {
+  name: ""
+}
+</script>
+
+<style scoped>
+
+</style>
